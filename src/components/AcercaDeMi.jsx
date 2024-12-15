84452279e05b3b811a17b222c19d85cb6a7cb75b
Martin Fuks <martinfukss@gmail.com>
2024-12-15 16:58:10 -0300

    hot fix

--- src/components/AcercaDeMi.jsx
+++ src/components/AcercaDeMi.jsx
@@ -58,8 +58,7 @@ const AcercaDeMi = () => {
             adaptan a sus necesidades individuales. <br />
             <br /> Cuento con un título de grado como licenciado en Finanzas,
             egresado de la Universidad Argentina de la Empresa (UADE). Además,
-            estoy finalizando mis estudios en la carrera de Economía, en la
-            misma Universidad. <br />
+            soy licenciado en Economía, egresado de la misma universidad. <br />
             <br /> Me apasiona enseñar para brindar al alumno las herramientas
             necesarias para aprender conceptos del área de Finanzas y Economía.
             Mis clases están diseñadas para ser interactivas y dinámicas,
